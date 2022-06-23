f2d59646d844803da076e97a787737ffc8e5aa8d
phuongndd <phuongndd@devcamp.edu.vn>
2022-06-23 23:42:14 +0700

    Init

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require ("express");
-const {timeRun} = require("./app/middle/middle");
+//const {timeRun} = require("./app/middle/middle");
 function routerPath (name) {
     return require(`./app/router/${name}`)
 }
@@ -9,7 +9,7 @@ const mongoose = require ('mongoose')
 const app = express ();
 const port = 5000;
 
-app.use (timeRun)
+//app.use (timeRun)
 app.use (express.json())
 app.use (express.urlencoded({
     extended:true
@@ -22,38 +22,39 @@ app.use (express.urlencoded({
 // });
 
 // const URL = 'mongodb+srv://mongo-user:<password>@cluster-mongo-test.ieqay.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-const URL = "mongodb+srv://phuongcellphones:Phuong93@cluster0.yq190va.mongodb.net/my-mongo-db-phuong?retryWrites=true&w=majority"
+// const URL = "mongodb+srv://phuongcellphones:Phuong93@cluster0.yq190va.mongodb.net/my-mongo-db-phuong?retryWrites=true&w=majority"
 
-const connectDB = async () => {
-  try {
-    await mongoose.connect(
-      URL,
-      { 
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-      }
-    )
+// const connectDB = async () => {
+//   try {
+//     await mongoose.connect(
+//       URL,
+//       { 
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true
+//       }
+//     )
 
-    console.log('Connected to mongoDB')
-  } catch (error) {
-    console.log(error)
-    process.exit(1)
-  }
-}
+//     console.log('Connected to mongoDB')
+//   } catch (error) {
+//     console.log(error)
+//     process.exit(1)
+//   }
+// }
 
-connectDB()
+// connectDB()
 
-app.use("/users",routerPath("userRouter"))
-app.use("/dices-detail-history",routerPath("diceHistoryRouter"))
-app.use("/prizes",routerPath("prizeRouter"))
-app.use("/vouchers",routerPath("voucherRouter"))
-app.use("/",routerPath("rollRouter"))
+// app.use("/users",routerPath("userRouter"))
+// app.use("/dices-detail-history",routerPath("diceHistoryRouter"))
+// app.use("/prizes",routerPath("prizeRouter"))
+// app.use("/vouchers",routerPath("voucherRouter"))
+// app.use("/",routerPath("rollRouter"))
 
 //app.use (express.static(`${__dirname}/view/homepage`))
 
 app.get("/",(req,res) => {
-    console.log("test")
-    res.sendFile(path.join(`${__dirname}/view/homepage/Project Lucky Dice - Bootstrap.html`))
+    //console.log("test")
+    // res.sendFile(path.join(`${__dirname}/view/homepage/Project Lucky Dice - Bootstrap.html`))
+    res.send ("Welcome to Heroku")
 })
 
 app.listen(port, () => {
