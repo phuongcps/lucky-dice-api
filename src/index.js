5e8e3a8c7288c548b8403f9683f80bc5e1daea9f
phuongndd <phuongndd@devcamp.edu.vn>
2022-07-06 20:23:06 +0700

    Change

--- src/index.js
+++ src/index.js
@@ -28,7 +28,7 @@ app.use("/users",routerPath("userRouter"))
 app.use("/dices-detail-history",routerPath("diceHistoryRouter"))
 app.use("/prizes",routerPath("prizeRouter"))
 app.use("/vouchers",routerPath("voucherRouter"))
-app.use("/",routerPath("rollRouter"))
+app.use("/roll",routerPath("rollRouter"))
 
 app.use (express.static(`${__dirname}/resources/view/homepage`))
 
